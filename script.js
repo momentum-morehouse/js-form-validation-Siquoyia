bfbaf162fd65e599f4d45d5700c76c6a91206c69
Siquoyia <70215872+Siquoyia@users.noreply.github.com>
2020-09-22 19:37:11 -0700

    js form

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log('Add validation!');
+let form = document.querySelector("form");
+// console.log({form}) to test page input-field
+
+form.addEventListener("submit", function(event) {
+  // console.log("submitted")} to test page
+event.preventDefault()
+  // if name is more than 4 characters in length return true for validation of page
+  let name = document.querySelector("#name");
+  if (name.value.length > 0){
+    console.log("valid");
+    name.parentElement.classList.add("input-valid");
+    console.log(name.classList);
+  }
+  else {
+    console.log("invalid");
+    name.parentElement.classList.add("input-invalid");
+  
+  // name.classList.add("input-invalid");
+  }
+  })
+  let car = document.querySelector("#car-field");
+  if (car-year.value.length > 0){
+    console.log("valid");
+    name.parentElement.classList.add("input-valid");
+    console.log(name.classList);
+  }
+  else {
+    console.log("invalid");
+    name.parentElement.classList.add("input-invalid");
+  
+  // name.classList.add("input-invalid");
+  }
+  
